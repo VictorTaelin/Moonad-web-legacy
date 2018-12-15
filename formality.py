732154793df65e985790908ea1fa8d6598dee1cd
Victor Maia <srvictormaia@gmail.com>
2018-12-15 18:18:28 -0200

    Complete compilation and decompilation

--- formality.py
+++ formality.py
@@ -50,11 +50,19 @@ class Typ:
     def to_string(self, scope):
         return "Type"
 
-    def to_net(self, net, var_ptrs):
+    def shift(self, enclose, inc):
+        return Typ()
+
+    def substitute(self, depth, value):
+        Typ()
+
+    def build_net(self, net, var_ptrs):
         typ_addr = net.alloc_node(3)
-        net.link_ports(Pointer(typ_addr, 1), Pointer(typ_addr, 1))
-        net.link_ports(Pointer(typ_addr, 2), Pointer(typ_addr, 2))
-        return Pointer(typ_addr, 0)
+        net.link_ports(Pointer(typ_addr, 0), Pointer(typ_addr, 2))
+        return Pointer(typ_addr, 1)
+
+    def infer(self, context):
+        return Typ()
 
 class All:
     def __init__(self, name, bind, body):
@@ -65,13 +73,22 @@ class All:
     def to_string(self, scope):
         return "{" + self.name + " : " + self.bind.to_string(scope) + "} " + self.body.to_string(scope.prepend(self.name))
 
-    def to_net(self, net, var_ptrs):
+    def shift(self, depth, inc):
+        return All(self.name, self.bind.shift(depth, inc), self.body.shift(depth + 1, inc)) 
+
+    def substitute(self, depth, value):
+        return All(self.name, self.bind.substitute(self, depth, value), self.body.substitute(self, depth + 1, value))
+
+    def build_net(self, net, var_ptrs):
         all_addr = net.alloc_node(2)
-        bind_ptr = self.bind.to_net(net, var_ptrs)
+        bind_ptr = self.bind.build_net(net, var_ptrs)
         net.link_ports(Pointer(all_addr, 1), bind_ptr)
-        body_ptr = Lam(self.name, self.bind, self.body).to_net(net, var_ptrs)
+        body_ptr = Lam(self.name, self.bind, self.body).build_net(net, var_ptrs)
         net.link_ports(Pointer(all_addr, 2), body_ptr)
         return Pointer(all_addr, 0)
+
+    def infer(self, context):
+        return Typ()
 
 class Lam: 
     def __init__(self, name, bind, body):
@@ -82,17 +99,25 @@ class Lam:
     def to_string(self, scope):
         return "[" + self.name + " : " + self.bind.to_string(scope) + "] " + self.body.to_string(scope.prepend(self.name))
 
+    def shift(self, depth, inc):
+        return Lam(self.name, self.bind.shift(depth, inc), self.body.shift(depth + 1, inc)) 
+
+    def substitute(self, depth, value):
+        return Lam(self.name, self.bind.substitute(self, depth, value), self.body.substitute(self, depth + 1, value))
+
     # TODO cant do this because missing names, can't know what variable binds to what lambda
-    def to_net(self, net, var_ptrs):
+    def build_net(self, net, var_ptrs):
         lam_addr = net.alloc_node(1)
         tup_addr = net.alloc_node(1)
         net.link_ports(Pointer(lam_addr, 1), Pointer(lam_addr, 1))
         net.link_ports(Pointer(lam_addr, 0), Pointer(tup_addr, 2))
-        bind_ptr = self.bind.to_net(net, var_ptrs)
+        bind_ptr = self.bind.build_net(net, var_ptrs)
         net.link_ports(Pointer(tup_addr, 1), bind_ptr)
-        body_ptr = self.body.to_net(net, var_ptrs.prepend(Pointer(lam_addr, 1)))
+        body_ptr = self.body.build_net(net, var_ptrs.prepend(Pointer(lam_addr, 1)))
         net.link_ports(Pointer(lam_addr, 2), body_ptr)
         return Pointer(tup_addr, 0)
+
+    #def infer(term, context):
 
 class App:
     def __init__(self, func, argm):
@@ -102,57 +127,60 @@ class App:
     def to_string(self, scope):
         return "(" + self.func.to_string(scope) + " " + self.argm.to_string(scope) + ")"
 
-    def to_net(self, net, var_ptrs):
+    def shift(self, depth, inc):
+        return App(self.func.shift(depth, inc), self.argm.shift(depth, inc))
+
+    def substitute(self, depth, value):
+        return App(self.func.substitute(depth, value), self.argm.substitute(depth, value))
+
+    def build_net(self, net, var_ptrs):
         app_addr = net.alloc_node(1)
         tup_addr = net.alloc_node(1)
         net.link_ports(Pointer(tup_addr, 2), Pointer(app_addr, 0))
         net.link_ports(Pointer(tup_addr, 1), Pointer(tup_addr, 1))
-        func_ptr = self.func.to_net(net, var_ptrs)
+        func_ptr = self.func.build_net(net, var_ptrs)
         net.link_ports(Pointer(tup_addr, 0), func_ptr)
-        argm_ptr = self.argm.to_net(net, var_ptrs)
+        argm_ptr = self.argm.build_net(net, var_ptrs)
         net.link_ports(Pointer(app_addr, 1), argm_ptr)
         return Pointer(app_addr, 2)
 
 class Var:
-    def __init__(self, indx):
-        self.indx = indx
+    def __init__(self, index):
+        self.index = index
 
     def to_string(self, scope):
-        name = scope.get(self.indx)
+        name = scope.get(self.index)
         if name is not None:
             return name
         else:
-            return "#" + str(self.indx - scope.len())
+            return "#" + str(self.index - scope.len())
 
-    def to_net(self, net, var_ptrs):
-        ptr = var_ptrs.get(self.indx)
+    def shift(self, depth, inc):
+        return self.index if self.index < depth else self.index + inc
+
+    def substitute(self, depth, value):
+        if self.index == depth:
+            return value.shift(0, depth)
+        else:
+            return Var(self.index - 1)
+
+    def build_net(self, net, var_ptrs):
+        ptr = var_ptrs.get(self.index)
         if ptr is None:
-            var_addr = net.alloc_node(self.indx + 1000 - var_ptrs.len())
+            var_addr = net.alloc_node(self.index + 1000 - var_ptrs.len())
             net.link_ports(Pointer(var_addr, 0), Pointer(var_addr, 2))
             return Pointer(var_addr, 1)
         else:
-            return var_ptrs.get(self.indx)
+            dups_ptr = net.enter_port(ptr)
+            if net.enter_port(ptr) == ptr:
+                return ptr
+            else:
+                dup_addr = net.alloc_node(2000 + ptr.addr)
+                net.link_ports(Pointer(dup_addr, 0), ptr)
+                net.link_ports(Pointer(dup_addr, 1), dups_ptr)
+                return Pointer(dup_addr, 2)
 
-class Cpy:
-    def __init__(self, nam0, nam1, term, body):
-        self.nam0 = nam0
-        self.nam1 = nam1
-        self.term = term
-        self.body = body
-
-    def to_string(self, scope):
-        return "(TODO: copy to_string)"
-
-    def to_net(self, net, var_ptrs):
-        dup_addr = net.alloc_node(2000)
-        net.link_ports(Pointer(dup_addr, 1), Pointer(dup_addr, 1))
-        net.link_ports(Pointer(dup_addr, 2), Pointer(dup_addr, 2))
-        term_ptr = self.term.to_net(net, var_ptrs)
-        net.link_ports(term_ptr, Pointer(dup_addr, 0))
-        body_ptr = self.body.to_net(net, var_ptrs.prepend(Pointer(dup_addr, 1)).prepend(Pointer(dup_addr, 2)))
-        return body_ptr
-
-def from_net(net, ptr, var_ptrs, dup_exit):
+def net_to_term(net, ptr, var_ptrs, dup_exit):
     label = net.nodes[ptr.addr].label
 
     # Lam / Var / App
@@ -160,27 +188,27 @@ def from_net(net, ptr, var_ptrs, dup_exit):
 
         # Lam
         if ptr.port == 0:
-            bind = from_net(net, net.enter_port(Pointer(ptr.addr, 1)), var_ptrs, dup_exit)
+            bind = net_to_term(net, net.enter_port(Pointer(ptr.addr, 1)), var_ptrs, dup_exit)
             ptr  = net.enter_port(Pointer(ptr.addr, 2))
-            body = from_net(net, net.enter_port(Pointer(ptr.addr, 2)), var_ptrs.prepend(Pointer(ptr.addr, 1)), dup_exit)
+            body = net_to_term(net, net.enter_port(Pointer(ptr.addr, 2)), var_ptrs.prepend(Pointer(ptr.addr, 1)), dup_exit)
             return Lam("x" + str(var_ptrs.len()), bind, body)
 
         # Var
         if ptr.port == 1:
-            (indx, _) = var_ptrs.find(lambda p: p == ptr)
-            return Var(indx)
+            (index, _) = var_ptrs.find(lambda p: p == ptr)
+            return Var(index)
 
         # App
         if ptr.port == 2:
-            argm = from_net(net, net.enter_port(Pointer(ptr.addr, 1)), var_ptrs, dup_exit)
+            argm = net_to_term(net, net.enter_port(Pointer(ptr.addr, 1)), var_ptrs, dup_exit)
             ptr  = net.enter_port(Pointer(ptr.addr, 0))
-            func = from_net(net, net.enter_port(Pointer(ptr.addr, 0)), var_ptrs, dup_exit)
+            func = net_to_term(net, net.enter_port(Pointer(ptr.addr, 0)), var_ptrs, dup_exit)
             return App(func, argm)
 
     # All
     if label == 2:
-        type = from_net(net, net.enter_port(Pointer(ptr.addr, 1)), var_ptrs, dup_exit)
-        body = from_net(net, net.enter_port(Pointer(ptr.addr, 2)), var_ptrs, dup_exit).body
+        type = net_to_term(net, net.enter_port(Pointer(ptr.addr, 1)), var_ptrs, dup_exit)
+        body = net_to_term(net, net.enter_port(Pointer(ptr.addr, 2)), var_ptrs, dup_exit).body
         return All("x" + str(var_ptrs.len()), type, body)
 
     # Typ
@@ -194,49 +222,42 @@ def from_net(net, ptr, var_ptrs, dup_exit):
     # Dup
     if label >= 2000:
         if ptr.port == 0:
-            print "dup 0"
-            return from_net(net, net.enter_port(Pointer(ptr.addr, dup_exit.head)), var_ptrs, dup_exit.tail)
+            return net_to_term(net, net.enter_port(Pointer(ptr.addr, dup_exit.head)), var_ptrs, dup_exit.tail)
         else:
-            #return Typ()
-            return from_net(net, net.enter_port(Pointer(ptr.addr, 0)), var_ptrs, dup_exit.prepend(ptr.port))
+            return net_to_term(net, net.enter_port(Pointer(ptr.addr, 0)), var_ptrs, dup_exit.prepend(ptr.port))
 
-def to_net(term):
+def term_to_net(term):
     net = Net()
     root_addr = net.alloc_node(0)
-    term_ptr = term.to_net(net, List())
+    term_ptr = term.build_net(net, List())
     net.link_ports(Pointer(root_addr, 0), Pointer(root_addr, 2)) 
     net.link_ports(Pointer(root_addr, 1), term_ptr)
     return (net, term_ptr)
 
 def reduce(term):
-    (net, _) = to_net(term)
+    (net, _) = term_to_net(term)
     net.reduce()
-    term = from_net(net, net.enter_port(Pointer(0, 1)), List()) 
+    term = net_to_term(net, net.enter_port(Pointer(0, 1)), List()) 
     return term
-
-#BOOL  = All("P", Typ(), All("t", Typ(), All("f", Typ(), Var(2))))
-#TRUE  = Lam("P", Typ(), Lam("t", Typ(), Lam("f", Typ(), Var(1))))
-#FALSE = Lam("P", Typ(), Lam("t", Typ(), Lam("f", Typ(), Var(0))))
-#NOT   = Lam("b", BOOL, App(App(App(Var(0), BOOL), FALSE), TRUE))
 
 ID = Lam("P", Typ(), Lam("x", Var(0), Var(0)))
 
-# (invalid, must copy P)
 NAT = All("P", Typ(),
       All("f", All("x", Var(0), Var(1)),
       All("x", Var(1),
       Var(2))))
 
-# (invalid, must copy P)
 TWO = Lam("P", Typ(),
       Lam("f", All("x", Var(0), Var(1)),
       Lam("x", Var(1),
-      Cpy("f0", "f1", Var(1),
-      App(Var(0), App(Var(1), Var(2)))))))
+      App(Var(0), App(Var(1), Var(2))))))
 
-term  = App(App(ID, NAT), TWO)
+ID  = Lam("x", Typ(), Var(0))
+FOO = Lam("f", Typ(), Lam("x", Typ(), App(Var(1), App(Var(1), App(Var(1), App(Var(1), App(Var(1), App(Var(1), App(Var(1), Var(0))))))))))
 
-(net, ptr) = to_net(term)
+term = App(App(App(FOO, FOO), ID), ID)
+
+(net, ptr) = term_to_net(term)
 #print net
 
 print "Input term:"
@@ -244,12 +265,12 @@ print term.to_string(List())
 print ""
 
 print "Recovered form net:"
-print from_net(net, net.enter_port(Pointer(0, 1)), List(), List()).to_string(List())
+print net_to_term(net, net.enter_port(Pointer(0, 1)), List(), List()).to_string(List())
 print ""
 
-net.reduce()
-#print net
+rewrites = net.reduce()
+print rewrites
 
 print "Reduced:"
-print from_net(net, net.enter_port(Pointer(0, 1)), List(), List()).to_string(List())
+print net_to_term(net, net.enter_port(Pointer(0, 1)), List(), List()).to_string(List())
 print ""
