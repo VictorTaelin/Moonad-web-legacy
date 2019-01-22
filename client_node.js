b2b42cc390e38370a9a6074015150531f33bbdd9
Leonardo Souza <leonardostsouza@gmail.com>
2019-01-22 16:57:55 -0200

    Implemented timestamp

--- client_node.js
+++ client_node.js
@@ -16,35 +16,86 @@ const REMOTE = parseInt(process.argv[3]);
 
 // useful variables
 var blockchain = new eth.Blockchain; // TODO: Save blockchain in a file
-blockchain.add(empty_block());
+blockchain.add(empty_block()); //TODO: testing only. Remove this line on release
 var socket = udp.createSocket('udp4');
 socket.bind(PORT);
+var timeOffset = [];
 
 // Peer Database
-var db = {'type':'addr', 'known':[]};
+var db = {'type':'addr', 'peers':[]};
 
 // Message Templates
+const requestConnJson = {'type':'requestConn', 'port':PORT, 'time':0};
+const acceptConnJson = {'type':'acceptConn', 'port':PORT, 'time':0};
 const getPeersJson = {'type':'getPeers', 'port':PORT};
 const getBlkJson = {'type':'getBlk', 'port':PORT, 'hash':eth.empty_hash};
 const getTipJson = {'type':'getTip', 'port':PORT};
 const sendBlockJson = {'type':'block', 'port':PORT, 'hash':eth.empty_hash, 'block':empty_block()};
 
-// Useful functions
+// Useful general functions
 function randomInt(max){
     return Math.trunc(Math.random() * max);
 }
 
 function randomPeer(){
-    return db.known[randomInt(db.known.length)];
+    return db.peers[randomInt(db.peers.length)];
 }
 
-function pushNewPort(newPort){
-    if ((!db.known.includes(newPort)) && (newPort != PORT)){
-        db.known.push(newPort);
+function median(numbers) {
+    var median = 0;
+    var numsLen = numbers.length;
+    numbers.sort();
+
+    if (numsLen > 0) {
+        if (numsLen % 2 === 0) {
+            // average of two middle numbers
+            median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2;
+        }
+        else {
+            // middle number only
+            median = numbers[(numsLen - 1) / 2];
+        }
+    }
+
+    return median;
+}
+
+// returns current elapsed time from Epoch (01 January 1970) in seconds
+function localTime() {
+    return Math.round(Date.now()/1000);
+}
+
+function networkAdjustedTime() {
+    /* Whenever a node connects to another node, it gets a UTC timestamp from it,
+    and stores its offset from node-local UTC. The network-adjusted time is then
+    the node-local UTC plus the median offset from all connected nodes.
+    Network time is never adjusted more than 70 minutes from local system
+    time, however.*/
+    var offset = median(timeOffset);
+    if (offset > 4200) {
+        offset = 4200;
+    }
+    else if (offset < -4200) {
+        offset = -4200;
+    }
+    return localTime() + offset;
+}
+
+// Useful network functions
+function peerIsNew(peer) {
+    return ((!db.peers.includes(peer)) && (peer != PORT));
+}
+
+function pushNewPeer(peer, peerTime) {
+    if (peerIsNew(peer)) {
+        var offset = localTime() - peerTime;
+        timeOffset.push(offset);
+        db.peers.push(peer);
+        console.log("Connection accepted. Time Offset = " + offset +
+        "\tNAT = " + networkAdjustedTime());
         console.log(db);
     }
 }
-pushNewPort(REMOTE);
 
 function sendMsg(msg, port) {
     socket.send(msg, port,'localhost', function(error){
@@ -54,6 +105,12 @@ function sendMsg(msg, port) {
             //console.log(msg + ' >>>>> ' + port);
         }
     });
+}
+
+function requestConnection(peer){
+    var msg = requestConnJson;
+    msg.time = localTime();
+    sendMsg(JSON.stringify(msg), peer);
 }
 
 function getPeers(remote) {
@@ -71,7 +128,7 @@ function getBlock(hash, remote) {
 }
 
 function broadcast(msg) {
-    db.known.forEach(function(peer, index) {
+    db.peers.forEach(function(peer, index) {
         sendMsg(msg, peer);
     });
 }
@@ -83,6 +140,26 @@ function broadcastBlock(blk){
     broadcast(JSON.stringify(msg));
 }
 
+function timestampIsValid(block){
+    /*A timestamp is accepted as valid if it is greater than the median
+    timestamp of previous 11 blocks, and less than the network-adjusted
+    time + 2 hours. "Network-adjusted time" is the median of the timestamps
+    returned by all nodes connected to you. */
+    var itt = 0;
+    var prevHash = block.prev_hash;
+    var prevBlk;
+    do {
+        prevBlk = blockchain.blocks[prevHash];
+        prevHash = prevBlk.prev_hash;
+        itt++;
+    } while ((itt < 6) && prevHash != undefined);
+
+    var medianTimestamp = prevBlk.time;
+    return (((block.time > medianTimestamp) &&
+             (block.time < networkAdjustedTime + 7200)) ||
+             (Object.keys(blockchain.blocks).length < 11));
+}
+
 // Event Handlers
 // emits on new datagram msg
 socket.on('message',function(msg, remote){
@@ -91,11 +168,28 @@ socket.on('message',function(msg, remote){
 
     switch(req.type){
         //---------------------------------------------------------------------
+        case requestConnJson.type:
+        // Another client requested a connection
+        console.log("Received connection request from " + req.port + " with time = " + req.time);
+        pushNewPeer(req.port, req.time);
+        var resp = acceptConnJson;
+        resp.time = localTime();
+        console.log("Sending response: " + JSON.stringify(resp));
+        sendMsg(JSON.stringify(resp), req.port);
+        break;
+
+        //---------------------------------------------------------------------
+        case acceptConnJson.type:
+        // Another client accepted my connection request
+        console.log("Connection accepted from " + req.port + " with time = " + req.time);
+        pushNewPeer(req.port, req.time);
+        break;
+
+        //---------------------------------------------------------------------
         case getPeersJson.type:
         // Another client requested list of peers
         var dbStr = JSON.stringify(db);
         sendMsg(dbStr, remote.port);
-        pushNewPort(remote.port);
         break;
 
         //---------------------------------------------------------------------
@@ -106,7 +200,6 @@ socket.on('message',function(msg, remote){
         msg.hash = req.hash;
 
         sendMsg(JSON.stringify(msg), remote.port);
-        pushNewPort(remote.port);
         break;
 
         //---------------------------------------------------------------------
@@ -117,34 +210,39 @@ socket.on('message',function(msg, remote){
         msg.hash = blockchain.get_tip();
 
         sendMsg(JSON.stringify(msg), remote.port);
-        pushNewPort(remote.port);
         break;
 
         //---------------------------------------------------------------------
         // Received response from getPeers
         case db.type:
-        req['known'].forEach(function(item, index){
-            pushNewPort(item);
+        req.peers.forEach(function(peer, index) {
+            // TODO: Implement a way to resend request in case a response does
+            // not arrive
+            if (peerIsNew(peer)) {
+                requestConnection(peer);
+            }
         });
         break;
 
         //---------------------------------------------------------------------
         // Received block
         case sendBlockJson.type:
-        var newBlock = new eth.Block;
-        newBlock.from_json(req.block);
-        // check if block is valid and not yet on local blockchain copy
-        if ((newBlock.is_valid()) &&
-            (newBlock.hash() === req.hash) &&
-            (!blockchain.blocks[req.hash])) {
-            // add block to blockchain
-            blockchain.add(newBlock);
-            // broadcast block to other peers
-            broadcastBlock(newBlock);
-            console.log("Blockchain len = " + Object.keys(blockchain.blocks).length);
-            // if previous block is not on local blockchain copy, request previous block
-            if(!blockchain.blocks[req.block.prev_hash]){
-                getBlock(req.block.prev_hash, remote.port);
+        if (req.block) {
+            var newBlock = new eth.Block;
+            newBlock.from_json(req.block);
+            // check if block is valid and not yet on local blockchain copy
+            if ((newBlock.is_valid()) &&
+                (newBlock.hash() === req.hash) &&
+                (!blockchain.blocks[req.hash])) {
+                // add block to blockchain
+                blockchain.add(newBlock);
+                // broadcast block to other peers
+                broadcastBlock(newBlock);
+                console.log("Blockchain len = " + Object.keys(blockchain.blocks).length);
+                // if previous block is not on local blockchain copy, request previous block
+                if(!blockchain.blocks[req.block.prev_hash]){
+                    getBlock(req.block.prev_hash, remote.port);
+                }
             }
         }
         break;
@@ -183,23 +281,23 @@ socket.on('close',function(){
 // getPeers
 setInterval(function() {
     //console.log("getPeers");
-    var remote = randomPeer();
-    getPeers(remote);
+    if (db.peers.length > 0){
+        var remote = randomPeer();
+        getPeers(remote);
+    }
+    else {
+        requestConnection(REMOTE);
+    }
 },5000);
 
 // getTip
 setInterval(function() {
     //console.log("getTip");
-    var remote = randomPeer();
-    getTip(remote);
+    if (db.peers.length > 0){
+        var remote = randomPeer();
+        getTip(remote);
+    }
 },6000);
-
-// getBlock
-/*setInterval(function() {
-    console.log("getBlock");
-    var remote = randomPeer();
-    getBlock(remote);
-},7000);*/
 
 // newBlock
 setInterval(function(){
@@ -213,14 +311,10 @@ setInterval(function(){
     broadcastBlock(blk);
 }, 10000);
 
-/*setInterval(function(){
-    console.log("Blockchain len = " + Object.keys(blockchain.blocks).length);
-}, 2000);*/
-
 // -------------- TEST FUNCTIONS ---------------- \\
 function empty_block(prev_hash, extra) {
     return new eth.Block(
-        "0000000000000000",
+        localTime().toString(),
         "0000000000000000",
         extra || "00000000000000000000000000000000",
         "0000000000000000000000000000000000000000000000000000000000000000",
